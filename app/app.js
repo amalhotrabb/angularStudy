229c8f534a93c799c4dc61cd141f0ceee453c285
amalhotrabb <amalhotra@beachbody.com>
2017-02-14 18:15:28 +0530

    Initial Commit

--- app/app.js
+++ app/app.js
@@ -1,14 +1,26 @@
-'use strict';
+// App Module: the name AngularStore matches the ng-app attribute
+// in the main <html> tag. The route provides parses the URL and
+// injects the appropriate partial page
+//localStorage.setItem("isUserLoggedIn", false);
+var myApp = angular.module('myApp', ['ngRoute', 'login','home','splash'])
+        //  .factory('testInterceptor', testInterceptor)
+        .config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
+//                $httpProvider.interceptors.push('testInterceptor');
+                $routeProvider.
+                        when('/', {
+                            templateUrl: 'splash/splash.html',
+                            controller: 'splashController',
+                            controllerAs: 'splashPage'}).
+                        when('/login', {
+                            templateUrl: 'login/login.html',
+                            controller: 'loginController',
+                            controllerAs: 'login'}).
+                        when('/home', {
+                            templateUrl: 'home/home.html',
+                            controller: 'homeController'
+                            , controllerAs: 'home'})
+                        .otherwise({
+                            redirectTo: '/'
+                        });
+            }]);
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
